c37a929c56b1af717841cdf9c502b0223a4c578d
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2025-06-10 14:44:42 +0100

    initialize project structure with Spring Boot, Maven wrapper, and essential configurations

---
+++ src/test/java/com/degital/skills/DegitalSkillsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.degital.skills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DegitalSkillsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
